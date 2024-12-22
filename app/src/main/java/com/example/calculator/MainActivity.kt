af87e99b0980c2d5968620c40406d720fb9f152e
Moin333 <ansarimoin7861@gmail.com>
2024-12-23 01:17:44 +0530

    UI Changes

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,8 +14,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.Backspace
+import androidx.compose.material.icons.filled.CloseFullscreen
+import androidx.compose.material.icons.filled.FullscreenExit
+import androidx.compose.material.icons.filled.GridView
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,7 +35,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.util.Stack
@@ -48,39 +63,96 @@ fun CalculatorUI() {
     Column (
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
-            .padding(16.dp),
+            .background(Color.Black),
         verticalArrangement = Arrangement.SpaceAround
     ) {
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(28.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Icon(imageVector = Icons.Default.FullscreenExit,
+                contentDescription = "Minimize Screen",
+                tint =  Color.White,
+                modifier = Modifier.size(40.dp)
+                )
+
+            Row {
+                Icon(imageVector = Icons.Default.AccessTime,
+                    contentDescription = "History",
+                    tint =  Color.White,
+                    modifier = Modifier.size(40.dp)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Icon(imageVector = Icons.Default.GridView,
+                    contentDescription = "History",
+                    tint =  Color.White,
+                    modifier = Modifier.size(40.dp)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Icon(imageVector = Icons.Filled.MoreVert,
+                    contentDescription = "History",
+                    tint =  Color.White,
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(140.dp))
+
         Text(
             text = input,
             fontSize = 56.sp,
             color = Color.White,
             fontWeight = FontWeight.Bold,
+            style = TextStyle(
+                textDirection = TextDirection.Rtl
+            ),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, top = 40.dp)
+                .padding(end = 20.dp, top = 40.dp)
                 .align(Alignment.End)
         )
-        
-        Spacer(modifier = Modifier.height(22.dp))
+
+
+        Spacer(modifier = Modifier.height(72.dp))
 
         val buttons = listOf(
-            listOf("7", "8", "9", "/"),
-            listOf("4", "5", "6", "*"),
-            listOf("1", "2", "3", "-"),
-            listOf("C", "0", "=", "+"),
+            listOf("C", "%","<-","/"),
+            listOf("7", "8", "9", "*"),
+            listOf("4", "5", "6", "-"),
+            listOf("1", "2", "3", "+"),
+            listOf("00", "0", ".", "="),
         )
 
-        for (row in buttons) {
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0x38383838))
             ){
-                for (button in row) {
-                    CalculatorButton(button) {
-                        input = handleButtonClick(button, input)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 16.dp)
+            ){
+                for (row in buttons) {
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ){
+                        for (button in row) {
+                            CalculatorButton(button) {
+                                input = handleButtonClick(button, input)
+                            }
+                        }
                     }
                 }
             }
@@ -90,16 +162,37 @@ fun CalculatorUI() {
 
 @Composable
 fun CalculatorButton(symbol: String, onClick: () -> Unit){
+    val buttonSizes = if (symbol in listOf("/", "*", "-", "+", "=")) {
+        Modifier.size(78.dp)
+    } else {
+        Modifier.size(90.dp)
+    }
+    val buttonColors = when {
+        symbol in listOf("/", "*", "-", "+") -> {
+            ButtonDefaults.buttonColors(
+                containerColor = Color.DarkGray,
+                contentColor = Color.White
+            )
+        }
+        symbol == "=" -> {
+            ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFF68B19),
+                contentColor = Color.White
+            )
+        }
+        else -> {
+            ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.White
+            )
+        }
+    }
     Button(
         onClick = onClick,
-        modifier = Modifier
-            .size(80.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.DarkGray,
-            contentColor = Color.White
-        )
+        modifier = buttonSizes,
+        colors = buttonColors
     ) {
-       Text(symbol, fontSize = 28.sp)
+       Text(symbol, fontSize = 32.sp)
     }
 }
 
@@ -183,3 +276,9 @@ fun evaluatePostfix(postfix: List<String>): Double {
     return stack.pop()
 }
 
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCalculatorUI(){
+    CalculatorUI()
+}
